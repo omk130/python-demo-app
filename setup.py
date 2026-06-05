75a9c786e2e3bfa59c36a2ab5a1b29ecb3692495
omk130 <mahajanavomva@gmail.com>
2026-06-05 08:24:50 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+# package configuration file for python project
+
+from setuptools import setup, find_packages
+
+setup(
+    name="python-demo-app",
+    version="1.0.0",
+    packages=find_packages(),
+)
